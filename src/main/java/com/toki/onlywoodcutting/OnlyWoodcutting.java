0503c3d936380e2979f2dcf690c096ffc93d00bc
Trever <49538247+TreverFuhrer@users.noreply.github.com>
2025-06-07 21:57:29 -0500

    Add functional woodcutter block

--- src/main/java/com/toki/onlywoodcutting/OnlyWoodcutting.java
+++ src/main/java/com/toki/onlywoodcutting/OnlyWoodcutting.java
@@ -1,24 +1,26 @@
 package com.toki.onlywoodcutting;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import com.toki.onlywoodcutting.block.WoodcutterBlock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class OnlyWoodcutting implements ModInitializer {
-	public static final String MOD_ID = "onlywoodcutting";
+    public static final String MOD_ID = "onlywoodcutting";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Block WOODCUTTER = new WoodcutterBlock(Block.Settings.copy(Blocks.STONECUTTER));
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+    @Override
+    public void onInitialize() {
+        Registry.register(Registries.BLOCK, new Identifier(MOD_ID, "woodcutter"), WOODCUTTER);
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID, "woodcutter"), new BlockItem(WOODCUTTER, new Item.Settings()));
+        LOGGER.info("Initialized Only Woodcutting");
+    }
 }
