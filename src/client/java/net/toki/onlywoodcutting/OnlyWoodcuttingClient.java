ca072a2958c0df3ccaac7efb02e15a3ee190ab54
toki <clevertrever31@gmail.com>
2025-06-10 21:14:36 -0500

    Created new 1.21.1 version

--- src/client/java/net/toki/onlywoodcutting/OnlyWoodcuttingClient.java
+++ src/client/java/net/toki/onlywoodcutting/OnlyWoodcuttingClient.java
@@ -3,10 +3,8 @@ package net.toki.onlywoodcutting;
 import net.fabricmc.api.ClientModInitializer;
 
 public class OnlyWoodcuttingClient implements ClientModInitializer {
-
-    @Override
-    public void onInitializeClient() {
-        
-    }
-    
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
 }
