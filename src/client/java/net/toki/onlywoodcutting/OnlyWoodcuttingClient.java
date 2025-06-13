56d808efa24abde1f7ef01ed82de0c0264be9b85
toki <clevertrever31@gmail.com>
2025-06-13 00:07:41 -0500

    Created new model and textures

--- src/client/java/net/toki/onlywoodcutting/OnlyWoodcuttingClient.java
+++ src/client/java/net/toki/onlywoodcutting/OnlyWoodcuttingClient.java
@@ -1,7 +1,10 @@
 package net.toki.onlywoodcutting;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.minecraft.client.render.RenderLayer;
+import net.toki.onlywoodcutting.block.ModBlocks;
 import net.toki.onlywoodcutting.screen.ModScreenHandlers;
 import net.toki.onlywoodcutting.screen.WoodcutterScreen;
 
@@ -10,5 +13,6 @@ public class OnlyWoodcuttingClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		HandledScreens.register(ModScreenHandlers.WOODCUTTER_SCREEN_HANDLER, WoodcutterScreen::new);
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.WOODCUTTER, RenderLayer.getCutout());
 	}
 }
